83007e001d82de7e37abf3bb302301a2e166fb4e
whizz75 <phokantsekhe@gmail.com>
2025-10-07 20:31:08 +0200

    ✨Added new client configurations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,34 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
-import Login from "@/features/auth/pages/Login"
-import Register from "@/features/auth/pages/Register"
-import StudentRoutes from "@/routes/StudentRoutes"
-import LecturerRoutes from "@/routes/LecturerRoutes"
-import PRLRoutes from "@/routes/PRLRoutes"
-import PLRoutes from "@/routes/PLRoutes"
-import { isAuthenticated } from "@/lib/auth"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Router>
-      <Routes>
-
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
-        <Route path="/student/*" element={<StudentRoutes />} />
-        <Route path="/lecturer/*" element={<LecturerRoutes />} />
-        <Route path="/prl/*" element={<PRLRoutes />} />
-        <Route path="/pl/*" element={<PLRoutes />} />
-
-        <Route
-          path="/"
-          element={
-            isAuthenticated() ? (
-              <Navigate to="/student/monitoring" replace />
-            ) : (
-              <Navigate to="/login" replace />
-            )
-          }
-        />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
+
+export default App
--- client/src/lib/endpoints/users.js
+++
@@ -1,5 +0,0 @@
-// lib/endpoints/users.js
-import api from "@/lib/api";
-import { ENDPOINTS } from "@/lib/constants";
-
-export const getCurrentUser = () => api.get(ENDPOINTS.USERS);
